d12f7fe538022ca6d6cdddbf9f498490d27f0c62
Jonathan Gardner <jgardner@pro.com>
2015-06-12 09:28:41 -0700

    Dialog works

--- jgrpgtoolkit.py
+++ jgrpgtoolkit.py
@@ -5,6 +5,7 @@ import sys
 from PyQt5.uic import loadUiType
 
 ui_MainWindow, MainWindowBaseClass = loadUiType('ui/MainWindow.ui')
+ui_CreateCharacterDialog, CreateCharacterDialogBaseClass = loadUiType('ui/CreateCharacterDialog.ui')
 
 from PyQt5.Qt import Qt
 
@@ -23,6 +24,17 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtWebKitWidgets import QWebView
 from PyQt5.QtCore import QUrl
 
+class CreateCharacterDialog(
+        CreateCharacterDialogBaseClass,
+        ui_CreateCharacterDialog
+):
+    
+    def __init__(self):
+        super(CreateCharacterDialog, self).__init__()
+
+        self.setupUi(self)
+
+
 class MainWindow(MainWindowBaseClass, ui_MainWindow):
     
     def __init__(self):
@@ -31,7 +43,13 @@ class MainWindow(MainWindowBaseClass, ui_MainWindow):
         self.setupUi(self)
 
     def createCharacter(self):
-        print("Create Character")
+        try:
+            dialog = self.createCharacterDialog
+        except AttributeError:
+            dialog = self.createCharacterDialog = CreateCharacterDialog()
+        dialog.show()
+        dialog.raise_()
+        dialog.activateWindow()
     
 
 if __name__ == '__main__':
